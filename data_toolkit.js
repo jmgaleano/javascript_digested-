3d695b860feee556f7378f03ff37ac94d007a791
unknown <jm.galeano1@gmail.com>
2019-11-24 20:33:47 -0500

    required packages for data collection

---
+++ data_toolkit.js
@@ -0,0 +1,11 @@
+/**
+ *  - Puppeter: Provides a high-level API to control Chrome over the DevTools Protocol. 
+ *  - Async-file: Adapts the Node.js File System API (fs) for use with TypeScript async/await.
+ *  - Randomstring: Library to help you create random strings.
+ *  - Request: Designed to be the simplest way possible to make http calls.
+ */
+const puppeteer = require("puppeteer");
+const fs = require('async-file');
+const randomstring = require("randomstring");
+const request = require("request");
+// const { readFile } = require('./path/to/.exports');
